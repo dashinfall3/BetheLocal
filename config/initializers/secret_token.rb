f4b792fd5797c6d28c63390b3ce280eefa1c4345
Chris Sell <christophersell3@gmail.com>
2012-05-11 14:18:01 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BetheLocal::Application.config.secret_token = '07a581dee4171890e875bc4233b8aee9282ae06727c38c4d4c80bcbd511e8d0c493bdfb027e9fd898fc35c08987a96ae1063cbdfec66ba716fe818ce2396d0f6'
